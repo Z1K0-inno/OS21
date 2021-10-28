4405259050fb834a0f3d71c68c28e752f4008e12
Z1K0-inno <69506311+Z1K0-inno@users.noreply.github.com>
2021-10-28 19:24:44 +0300

    Update ex2.sh

--- week10/ex2.sh
+++ week10/ex2.sh
@@ -1,6 +1,6 @@
-echo zeiin > ../week01/file.txt
-link ../week01/file.txt _ex2.txt
-ls -i ../week01/file.txt
+echo zeiin > ../week1/file.txt
+link ../week1/file.txt _ex2.txt
+ls -i ../week1/file.txt
 find -inum 811347
 find -inum 811347 -exec rm {} \;
 find -inum 811347
