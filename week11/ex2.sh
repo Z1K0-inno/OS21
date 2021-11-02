1ff085f7ab50d6c97159ee310e66abf3a9344d67
Z1K0-inno <69506311+Z1K0-inno@users.noreply.github.com>
2021-11-02 23:46:51 +0300

    Add files via upload

---
+++ week11/ex2.sh
@@ -0,0 +1,29 @@
+chr=`pwd`/lofsdisk
+
+sudo mkdir -p $chr/usr/bin/
+sudo mkdir -p $chr/bin/
+sudo mkdir -p $chr/lib64/
+
+sudo cp -v /bin/bash $chr/bin/bash
+sudo cp -v /bin/ls $chr/bin/ls
+sudo cp -v /bin/cat $chr/bin/cat
+sudo cp -v /bin/echo $chr/bin/echo
+
+for cmd in /bin/bash /bin/ls /bin/cat /bin/echo
+do
+	list=$(ldd $cmd | egrep -o "/lib.*\.[0-9]")
+	IFS=$`\n`
+	echo $list | while read -r i; do sudo cp -v --parents "$i" "${chr}"; done
+done
+
+gcc ex2.c -o ex2.out
+
+sudo bash -c "echo 'Zeiin' > ${chr}/file1"
+sudo bash -c "echo 'Kanabekov' > ${chr}/file2"
+
+sudo cp ex2.out $chr/ex2.out
+
+echo "chroot" > ex2.txt
+sudo chroot $chr ./ex2.out >> ex2.txt 
+echo "actual root" >> ex2.txt
+./ex2.out >> ex2.txt
